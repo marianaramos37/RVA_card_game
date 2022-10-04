2eb65c91703d55d88f95648c6ccc3e70e952a3f6
marianaramos37 <7marianaramos@gmail.com>
2022-10-04 19:35:21 +0100

    calibrating camera for homography

--- card_detector.py
+++ card_detector.py
@@ -1,4 +1,61 @@
 import cv2
 import numpy as np
-import time
-import os
+import glob
+
+# https://learnopencv.com/camera-calibration-using-opencv/
+
+def calibrate_camera(img):
+    # Termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    objp = np.zeros((6*9, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+
+    images = glob.glob('images/chessboard_calibration/*.jpg')
+
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+
+        if ret:
+            objpoints.append(objp)
+
+            cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners)
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    return ret, mtx, 
+
+# Binarize an image
+def binarize_image(original_image):
+    gray_scale_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    _, binary_image = cv2.threshold(gray_scale_image, 127, 255, cv2.THRESH_BINARY)
+    return binary_image
+
+def main():
+    cap = cv2.VideoCapture(0)
+    cards_normal = glob.glob('images/cards_normal/*.png')
+
+    while True:
+        _, frame = cap.read()
+
+        # Calibrate camera
+
+        ret, mtx, dist = calibrate_camera(frame)
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
+
+main()
