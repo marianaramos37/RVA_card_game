7b7fd9e31bc78d2c5178ee26c37a5e79e9e848e0
marianaramos37 <7marianaramos@gmail.com>
2022-10-09 21:44:05 +0100

    card recognition working on simple cards, game logic

--- card_detector.py
+++ card_detector.py
@@ -1,29 +1,41 @@
 import cv2
 import numpy as np
 import glob
-from skimage.metrics import structural_similarity as ssim 
+from skimage.metrics import structural_similarity as ssim
+import sys
+
+from camera_calibration import calibrate_camera 
+import game_logic
+
 
 NUM_CARDS = 4
+CARD_MAX_AREA = 18000
+CARD_MIN_AREA = 6000
 
 ####### Compare captured contours with card of the database
 
-def compare_contours(captured_image, cards_corners, cards_normal):
+def compare_contours(captured_image, cards_corners, cards_db_names, cards_db):
 
     # Get corners and dimensions of the card on the database
-    reference_image = cv2.imread(cards_normal[0])
-    height, width = reference_image.shape[:-1]
+    height, width = cards_db[0].shape[:2]
 
     # Initial original image and dst corners
     original = np.array(cards_corners, np.float32)
     dst = np.array([[0, 0], [0, height - 1], [width - 1, height - 1], [width - 1, 0]], np.float32)
 
-    similaritys = []
+    less_difference = 230
+    best_match = ""
+
     # For every rotation (horizontal or vertical)
-    for i in range(2):
+    for i in range(4):
 
         # Rotate real image
         if i == 1:
             original = np.array([cards_corners[1], cards_corners[2], cards_corners[3], cards_corners[0]], np.float32)
+        if i == 2:
+            original = np.array([cards_corners[2], cards_corners[3], cards_corners[0], cards_corners[1]], np.float32)
+        if i == 3:
+            original = np.array([cards_corners[3], cards_corners[0], cards_corners[1], cards_corners[2]], np.float32)
 
         # Compute homography
         matrix = cv2.getPerspectiveTransform(original, dst)
@@ -32,19 +44,18 @@ def compare_contours(captured_image, cards_corners, cards_normal):
         card_frontal_view = cv2.warpPerspective(captured_image, matrix, (width, height))    
 
         # Compare images with cards on the database
-        for card in cards_normal:
-            card_of_the_database = binarize_image(cv2.imread(card))
-            similarity = compute_similarity(card_frontal_view, card_of_the_database)
-            if similarity > 0.88:
-                return card
-
-
+        for c in range(len(cards_db_names)):
+            similarity = compute_similarity(card_frontal_view, cards_db[c])
+            if similarity < less_difference:
+                less_difference = similarity
+                best_match = cards_db_names[c]
+        
+    return best_match
 
 ####### Get the corners of a card through its countor 
 
 def get_corners(contour):
     return cv2.approxPolyDP(contour, 0.1*cv2.arcLength(contour, True), True)
-
 
 
 ####### Compare 2 images of the same size
@@ -68,12 +79,17 @@ def compute_similarity(image1, image2):  # Can we use any openCV function ?
     '''
 
     # Using norm():
-    
-    height, width = image1.shape
-    errorL2 = cv2.norm( image1, image2, cv2.NORM_L2 )
-    similarity = 1 - errorL2 / ( height * width )
-    
+    '''
+    height1, width1 = image1.shape
+    errorL2 = cv2.norm( image1, image2, cv2.NORM_L2 ) 
+    similarity = 1 - errorL2 / ( height1 * width1 )
+    '''
 
+    # Using abs diff
+    
+    diff_img = cv2.absdiff(image1, image2)
+    similarity = int(np.sum(diff_img)/255)
+    
     # By pixel, too slow:
     '''
     rest = cv2.bitwise_xor(image1, image2)
@@ -105,128 +121,141 @@ def compute_similarity(image1, image2):  # Can we use any openCV function ?
     similarity = c1**(1 / 2)
     '''
     return similarity
-    
-
-
-####### Calibrate camera with chessboard images
-####### https://learnopencv.com/camera-calibration-using-opencv/
-
-def calibrate_camera(img):
-    # Termination criteria
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-    objp = np.zeros((6*9, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
-
-    # Arrays to store object points and image points from all the images.
-    objpoints = [] # 3d point in real world space
-    imgpoints = [] # 2d points in image plane.
-
-    images = glob.glob('images/chessboard_calibration/*.jpg')
-
-    for fname in images:
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-        # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
-
-        if ret:
-            objpoints.append(objp)
-
-            cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-            imgpoints.append(corners)
-
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    return ret, mtx, 
-
 
 
 ####### Binarize an image
 
 def binarize_image(original_image):
     gray_scale_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    _, binary_image = cv2.threshold(gray_scale_image, 127, 255, cv2.THRESH_BINARY)
+    img_w, img_h = np.shape(original_image)[:2]
+
+    bkg_level = gray_scale_image[int(img_h/100)][int(img_w/2)]
+    thresh_level = bkg_level + 130 
+
+    _, binary_image = cv2.threshold(gray_scale_image, thresh_level, 255, cv2.THRESH_BINARY)
     return binary_image
 
+# Finds all card-sized contours in a thresholded camera image. Returns the number of cards, and a list of card contours sorted from largest to smallest.
 
+def find_cards(thresh_image):
 
-####### Do card recognition on just a test image - To be deleted once it is working on the main function
+    # Find contours and sort their indices by contour size
+    cnts,hier = cv2.findContours(thresh_image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    index_sort = sorted(range(len(cnts)), key=lambda i : cv2.contourArea(cnts[i]),reverse=True)
 
-def test_on_image():
-    cards_normal = glob.glob('./images/cards_normal/*')
+    if len(cnts) == 0:
+        return [], []
+    
+    cnts_sort = [] # List of sorted contours
+    hier_sort = []
+    cnt_is_card = np.zeros(len(cnts),dtype=int) # [0,1,0,1,1,...] where 1 is card 
 
-    test_image = cv2.imread('./images/test_image2.png')
-    test_image = cv2.resize(test_image,(500,500))
+    for i in index_sort:
+        cnts_sort.append(cnts[i])
+        hier_sort.append(hier[0][i])
 
-    # Binarize 
-    binary_image = binarize_image(test_image)
+    for i in range(len(cnts_sort)):
+        size = cv2.contourArea(cnts_sort[i])
+        peri = cv2.arcLength(cnts_sort[i],True)
+        approx = cv2.approxPolyDP(cnts_sort[i],0.01*peri,True)
+        
+        if ((size < CARD_MAX_AREA) and (size > CARD_MIN_AREA)
+            and (hier_sort[i][3] == -1) and (len(approx) == 4)):
+            cnt_is_card[i] = 1
 
-    contours, hierarchy = cv2.findContours(binary_image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea,reverse=True)[:NUM_CARDS]  
-
-    cv2.drawContours(test_image, contours, -1, (255, 0, 0), 3)
- 
-    card_names = []
-
-    for card_contour in contours:
-        corners = get_corners(card_contour)
-        x=0
-        y=0
-        for corner in corners:
-            x,y = corner.ravel()
-            cv2.circle(test_image, (x,y), 1, (0,0,255), cv2.LINE_AA)
-            
-        # Compute similarity and return name of each card
-        original = compare_contours(binary_image, get_corners(card_contour), cards_normal)
-
-        if original != None:
-            cv2.putText(test_image,original[-14:],(x-100,y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 1, cv2.LINE_AA)
-
-        card_names.append(original)
-
-    print(card_names)
-
-    cv2.imshow('test', test_image)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    return cnts_sort, cnt_is_card
 
 
 
 ####### Video capture main loop
 
 def main():
-    cap = cv2.VideoCapture(0)
-    cards_normal = glob.glob('images/cards_normal/*.png')
+
+    # Get trunfo
+    trunfo = ""
+    args = sys.argv[1:]
+    if(len(args) != 0 and (args[0]=="copas" or args[0]=="paus" or args[0]=="espadas" or args[0]=="ouros")):
+        trunfo = args[0]
+        print("Starting the SUECA game. The trunfo resgistered is " + trunfo + ". Loading the cards... this might take a minute.")
+    else:
+        print("Please specify a correct value for the 'trunfo' card.")
+        print("USAGE: card_detector.py [TRUNFO]")
+        print("Where  [TRUNFO] can be one of the following: 'copas', 'paus', 'ouros', 'espadas'")
+        return 
+
+    cap = cv2.VideoCapture(1)
+
+    # Pre-process database cards:
+
+    cards_normal = glob.glob('images/cards_simple/*.jpg')
+    cards_db_preprocessed = []
+
+    for card_name in cards_normal:
+        image = cv2.imread(card_name)
+        gray_scale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, binary_image = cv2.threshold(gray_scale_image, 127, 255, cv2.THRESH_BINARY)
+        cards_db_preprocessed.append(binary_image)
+
+    # Calibrate camera:
+    # ret, cameraMatrix = calibrate_camera()
+    
+    # Game Logic:
+
+    assistir = ""
+    card_team1_player1 = ""
+    card_team1_player2 = ""
+    card_team2_player1 = ""
+    card_team2_player2 = ""
+    number_of_cards_on_table = 0
 
     while True:
         _, frame = cap.read()
 
-        # Calibrate camera
-        # _, cameraMatrix, distortionCoefficients = calibrate_camera(frame)
-
         # Binarize 
         binary_frame = binarize_image(frame)
+        
+        cnts_sort, cnt_is_card = find_cards(binary_frame)
+        
+        num_cards_on_table = np.count_nonzero(cnt_is_card==1)
+        
+        cv2.putText(frame,"Trunfo: " + trunfo,(5,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame,"Team 1: " + trunfo,(5,40), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame,"Team 2: " + trunfo,(5,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2, cv2.LINE_AA)
 
-        contours, hierarchy = cv2.findContours(binary_frame,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        contours = sorted(contours, key=cv2.contourArea,reverse=True)[:NUM_CARDS]  
+        for c in range(len(cnts_sort)):
+            if(cnt_is_card[c]==1):
 
-        cv2.drawContours(frame, contours, -1, (255, 0, 0), 3)
+                corners = get_corners(cnts_sort[c])
+                    
+                team = game_logic.find_team(corners)
+                if(team=="team1"):
+                    cv2.drawContours(frame, cnts_sort[c], -1, (255, 0, 0), 3)  
+                else:
+                    cv2.drawContours(frame, cnts_sort[c], -1, (0, 255, 0), 2)
 
-        for card in contours:
-            corners = get_corners(card)
-            for corner in corners:
-                x,y = corner.ravel()
-                cv2.circle(frame, (x,y), 1, (0,0,255), cv2.LINE_AA)
+                # Compute similarity and return name of each card
+                original = compare_contours(binary_frame, corners, cards_normal, cards_db_preprocessed)
+
+                if original != None:
+                    cv2.putText(frame,original[20:][:-4],corners[0][0], cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2, cv2.LINE_AA)
+
+                if num_cards_on_table==1:
+                    assistir = original[20:][:-4]
+
+                if num_cards_on_table!=4:
+                    cv2.putText(frame,"Assistir: " + assistir,(5,80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2, cv2.LINE_AA)
+
+                if num_cards_on_table==4:
+                    #winning_team, winning_team_points = game_logic.load_game_logic(trunfo, assistir, card_team1_player1, card_team1_player2, card_team2_player1, card_team2_player2)
+                    cv2.putText(frame,"Terminou a ronda",(5,80), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2, cv2.LINE_AA)
 
         cv2.imshow('frame', frame)
-        
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
+
     cap.release()
     cv2.destroyAllWindows()
 
-test_on_image()
-#main()
+#test_on_image()
+main()
---
+++ game_logic.py
@@ -0,0 +1,57 @@
+from tkinter.tix import TList
+import numpy as np
+
+
+def load_game_logic(trunfo,assistir,card_t1_p1,card_t1_p2,card_t2_p1,card_t2_p2):
+    winning_team = ""
+    winning_team_points = 0 
+
+    naipe = assistir[:4] 
+    if card_t1_p1[:4]==naipe and card_t1_p2[:4]==naipe and card_t2_p1[:4]==naipe and card_t2_p2[:4]==naipe:
+        poits_team1 = get_points(card_t1_p1)+get_points(card_t1_p2)
+        poits_team2 = get_points(card_t2_p1)+get_points(card_t2_p1)
+        if(poits_team1 > poits_team2):
+            winning_team = "team1"
+            winning_team_points = poits_team1
+        else:
+            winning_team = "team2"
+            winning_team_points = poits_team2
+    #if apenas uma equipa jogar trunfo
+        # pontos vao todos para essa equipa
+    #if ambas as equipas jogarem trunfos   
+        # pontos vao para a equipa com trunfo maior
+
+    return winning_team, winning_team_points
+
+def find_team(card_corners):
+    # top left point: min(x+y)
+    tl = sorted(card_corners, key=lambda p: (p[0][0]) + (p[0][1]))[0] 
+    # top right point: max(x-y)
+    tr = sorted(card_corners, key=lambda p: (p[0][0]) - (p[0][1]))[-1]
+    corners_rest = np.delete(card_corners, np.where(card_corners == tl), axis=0)
+    corners_rest = np.delete(card_corners, np.where(card_corners == tr), axis=0)
+
+    bl = sorted(corners_rest, key=lambda p: (p[0][1]))[0] 
+    
+    # Card is vertical
+    if (bl[0][1] - tl[0][1]) > (tr[0][0] - tl[0][0]):
+        return "team2"
+    else:  # Card is horizontal
+        return "team1"
+        
+
+
+def get_points(card):
+    points = 0
+    if card[0]=="a":
+        points = 11
+    elif card[0]=="7":
+        points = 10
+    elif card[0]=="r":
+        points = 4
+    elif card[0]=="v":
+        points = 3
+    elif card[0]=="d":
+        points = 2
+    else: points = 0
+    return points
