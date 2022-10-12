b51a927757296796c0c008f3ee3d71b07b95a255
DanielGSilva <dg.sarmento2000@gmail.com>
2022-10-12 10:41:58 +0100

    Implemented game logic with game rules

--- game_logic.py
+++ game_logic.py
@@ -3,32 +3,44 @@ import numpy as np
 
 
 def load_game_logic(trunfo,assistir,card_t1_p1,card_t1_p2,card_t2_p1,card_t2_p2):
-    winning_team = "team1"
+    winning_team = ""
+    usedTrunfo = False
+    cards_played = [card_t1_p1, card_t1_p2, card_t2_p1, card_t2_p2]
+    cards_to_consider = np.zeros(4, dtype=int)
+    points = get_points(card_t1_p1)+get_points(card_t1_p2)+get_points(card_t2_p1)+get_points(card_t2_p2)
+    suit = getSuit(assistir)
     
-    points_team1 = get_points(card_t1_p1)+get_points(card_t1_p2)
-    points_team2 = get_points(card_t2_p1)+get_points(card_t2_p2)
+    if (getSuit(card_t1_p1) == trunfo):
+        cards_to_consider[0] = 1
+        usedTrunfo = True
+    if (getSuit(card_t1_p2) == trunfo):
+        cards_to_consider[1] = 1
+        usedTrunfo = True
+    if (getSuit(card_t2_p1) == trunfo):
+        cards_to_consider[2] = 1
+        usedTrunfo = True
+    if (getSuit(card_t2_p2) == trunfo):
+        cards_to_consider[3] = 1
+        usedTrunfo = True
 
-    naipe = assistir[:4] 
+    if (usedTrunfo == False):
+        if (getSuit(card_t1_p1) == suit):
+            cards_to_consider[0] = 1
+        if (getSuit(card_t1_p2) == suit):
+            cards_to_consider[1] = 1
+        if (getSuit(card_t2_p1) == suit):
+            cards_to_consider[2] = 1
+        if (getSuit(card_t2_p2) == suit):
+            cards_to_consider[3] = 1
+    
+    bestCard = ""
+    for i in range(4):
+        if (cards_to_consider[i] == 1 and (bestCard == "" or isBetter(cards_played[i], bestCard))):
+            bestCard = cards_played[i]
+            if (i < 2): winning_team == "team1"
+            else: winning_team == "team2"
 
-    if(points_team1 > points_team2):
-        winning_team = "team1"
-    else:
-        winning_team = "team2"
-
-    # Todas as equipas assistirem
-    #if card_t1_p1[:4]==naipe and card_t1_p2[:4]==naipe and card_t2_p1[:4]==naipe and card_t2_p2[:4]==naipe:
-    #    poits_team1 = get_points(card_t1_p1)+get_points(card_t1_p2)
-    #    poits_team2 = get_points(card_t2_p1)+get_points(card_t2_p1)
-    #    if(poits_team1 > poits_team2):
-    #        winning_team = "team1"
-    #    else:
-    #        winning_team = "team2"
-    #if apenas uma equipa jogar trunfo
-        # pontos vao todos para essa equipa
-    #if ambas as equipas jogarem trunfos   
-        # pontos vao para a equipa com trunfo maior
-
-    return winning_team, points_team1, points_team2
+    return winning_team, points
 
 def find_team(card_corners):
     if len(card_corners)==4:
@@ -49,8 +61,6 @@ def find_team(card_corners):
         else:  # Card is horizontal
             return "team1"
             
-
-
 def get_points(card):
     points = 0
     card=card[20:]
@@ -67,3 +77,29 @@ def get_points(card):
             points = 2
         else: points = 0
     return points
+
+def getSuit(firstCard):
+    termination = firstCard[-7:-4]
+    suit = ""
+    if (termination == "das"):
+        suit = "espadas"
+    elif (termination == "aus"):
+        suit = "paus"
+    elif (termination == "pas"):
+        suit = "copas"
+    elif (termination == "ros"):
+        suit = "ouros"
+    return suit
+    
+def isBetter(cardA, cardB):
+    if (getValue(cardA) >= getValue(cardB)): return True
+    return False
+    
+def getValue(card):
+    if (card[0] == "a"): return 15
+    if (card[0] == "7"): return 14
+    if (card[0] == "r"): return 13
+    if (card[0] == "v"): return 12
+    if (card[0] == "d"): return 11
+    return int(card[0])
+    
