b3284662eef337bcba7e47cd9f2b6aacec78ce30
marianaramos37 <7marianaramos@gmail.com>
2022-10-17 14:57:05 +0100

    find marker function

--- projection.py
+++ projection.py
@@ -2,36 +2,47 @@ import numpy as np
 import cv2 as cv
 import glob
 
-from camera_calibration import calibrate_camera 
-
-_, mtx, dist, _, _ = calibrate_camera()
-
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-objp = np.zeros((9*6,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
-axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
-                   [0,0,-10],[0,3,-10],[3,3,-10],[3,0,-10] ])
+objp = np.zeros((1*4,3), np.float32)
+objp[:,:2] = np.mgrid[0:2,0:2].T.reshape(-1,2)
+
+axis = np.float32([[0.5,0,0], [0,0.5,0], [0,0,-0.5]]).reshape(-1,3) # (x,y,z)
+
+cube = np.float32([[0,0,0], [0,0.5,0], [0.5,0.5,0], [0.5,0,0],
+                   [0,0,-0.5],[0,0.5,-0.5],[0.5,0.5,-0.5],[0.5,0,-0.5] ])
                    
-def draw_trophy(frame, corners):
+def draw_trophy(frame, corners, mtx, dist):
+
+    # mtx = [[focal_length, 0, center[0]],
+    #        [0, focal_length, center[1]],
+    #        [0, 0, 1]]
+    
+    corners = np.array(corners, np.float32)
+
     gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
 
-    corners2 = cv.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+    if(len(corners)>0):
+        corners2 = cv.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
 
-    ret, rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist)
+        # Find the rotation and translation vectors.
+        _, rvecs, tvecs,_ = cv.solvePnPRansac(objectPoints=objp, imagePoints=corners2, cameraMatrix=mtx, distCoeffs=dist)
 
-    imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
+        # project 3D points to image plane
+        imgpts, _ = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
 
-    imgpts = np.int32(imgpts).reshape(-1,2)
+        imgpts = np.int32(imgpts).reshape(-1,2)
 
-    # draw ground floor in green
-    img = cv.drawContours(frame, [imgpts[:4]],-1,(0,255,0),-3)
+        corners = np.array(corners, np.int32)
 
-    # draw pillars in blue color
-    for i,j in zip(range(4),range(4,8)):
-        frame = cv.line(frame, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
-       
-    # draw top layer in red color
-    frame = cv.drawContours(frame, [imgpts[4:]],-1,(0,0,255),3)
+        cv.circle(frame,tuple(corners[0].ravel()),1,[0,255,255],2)
+        cv.circle(frame,tuple(corners[1].ravel()),1,[0,255,255],2)
+        cv.circle(frame,tuple(corners[2].ravel()),1,[0,255,255],2)
+        cv.circle(frame,tuple(corners[3].ravel()),1,[0,255,255],2)
 
-    return img
+        corner = tuple(corners[0].ravel())
+        cv.line(frame, corner, tuple(imgpts[0].ravel()), (255,0,0), 2)
+        cv.line(frame, corner, tuple(imgpts[1].ravel()), (0,255,0), 2)
+        cv.line(frame, corner, tuple(imgpts[2].ravel()), (0,0,255), 2)
+
+        return
